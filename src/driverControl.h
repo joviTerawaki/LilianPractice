78582377dfd756da80c67b68d661a9298b05b346
jobo the hobro <jovi.terawaki@gmail.com>
2023-07-28 10:19:31 -1000

    init commit

---
+++ src/driverControl.h
@@ -0,0 +1,64 @@
+#include "vex.h"
+
+//TELEOP
+void driverControl (){
+  frontLeft.spin(forward, Controller1.Axis3.value(), pct);
+  frontRight.spin(forward, Controller1.Axis2.value(), pct);
+  backLeft.spin(forward, Controller1.Axis3.value(), pct);
+  backRight.spin(forward, Controller1.Axis2.value(), pct);
+}
+
+//AUTONOMOUS 
+void moveForward (int speed, int enc){
+  while(frontLeft.position(degrees) <= enc){
+    frontLeft.spin(forward, speed, pct);
+    frontRight.spin(forward, speed, pct);
+    backLeft.spin(forward, speed, pct);
+    backRight.spin(forward, speed, pct);
+
+  }
+    frontLeft.stop();
+    frontRight.stop();
+    backLeft.stop();
+    backRight.stop();
+}
+
+void moveBackward (int speed, int enc){
+  while(frontLeft.position(degrees) >= -enc){
+    frontLeft.spin(reverse, speed, pct);
+    frontRight.spin(reverse, speed, pct);
+    backLeft.spin(reverse, speed, pct);
+    backRight.spin(reverse, speed, pct);
+
+  }
+    frontLeft.stop();
+    frontRight.stop();
+    backLeft.stop();
+    backRight.stop();
+}
+
+void turnRight (int speed, int enc){
+  while(frontLeft.position(degrees)<= enc){
+    frontLeft.spin(forward, speed, pct);
+    frontRight.spin(reverse, speed, pct);
+    backLeft.spin(forward, speed, pct);
+    backRight.spin(reverse, speed, pct);
+  }
+    frontLeft.stop();
+    frontRight.stop();
+    backLeft.stop();
+    backRight.stop();  
+}
+
+void turnLeft (int speed, int enc){
+  while(frontLeft.position(degrees)>= -enc){
+    frontLeft.spin(reverse, speed, pct);
+    frontRight.spin(forward, speed, pct);
+    backLeft.spin(reverse, speed, pct);
+    backRight.spin(forward, speed, pct);
+  }
+    frontLeft.stop();
+    frontRight.stop();
+    backLeft.stop();
+    backRight.stop();  
+}
---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\robotics                                         */
+/*    Created:      Wed Jul 26 2023                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// frontRight           motor         1               
+// frontLeft            motor         2               
+// backRight            motor         3               
+// backLeft             motor         4               
+// Controller1          controller                    
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+#include "driverControl.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
